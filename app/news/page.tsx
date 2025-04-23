5155425455473f35aee7d55ed65fe0f1055dd620
Yusuke Tanaka <yusuke.tanaka4@persol.co.jp>
2025-04-23 14:23:39 +0900

    [修正][main]ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,8 @@ import Pagenation from "../_components/Pagenation";
 import SearchField from "../_components/SearchField";
 import {NEWS_LIST_LIMIT} from "../_constants";
 
+export const revalidate = 0;
+
 export default async function Page() {
   const {contents: news, totalCount} = await getNewsList({
     limit: NEWS_LIST_LIMIT,
