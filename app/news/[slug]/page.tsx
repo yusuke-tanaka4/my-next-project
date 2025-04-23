c24b6ed75a8abcc694823cf3b897b1b68e1d9643
Yusuke Tanaka <yusuke.tanaka4@persol.co.jp>
2025-04-23 10:48:04 +0900

    [修正][main]6章まで終了

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,4 +1,8 @@
+import {notFound} from "next/navigation";
 import {getNewsDetail} from "../../_libs/microcms";
+import Article from "../../_components/Article";
+import ButtonLink from "../../_components/ButtonLink";
+import styles from "./page.module.css";
 
 type Props = {
   params: {
@@ -7,7 +11,14 @@ type Props = {
 };
 
 export default async function Page({params}: Props) {
-  const data = await getNewsDetail(params.slug);
+  const data = await getNewsDetail(params.slug).catch(notFound);
 
-  return <div>{data.title}</div>;
+  return (
+    <>
+      <Article data={data} />
+      <div className={styles.footer}>
+        <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
+      </div>
+    </>
+  );
 }
